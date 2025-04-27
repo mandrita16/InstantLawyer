c418ca77bd9e57556ba661984df9754e61e2753d
Mandrita Dasgupta <mandritadasgupta16@gmail.com>
2025-04-28 00:10:15 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 import http from 'http';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
-const fs = require('fs');
-const path = require('path');
+// To replace __dirname in ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const PORT = process.env.PORT || 3000;
 
